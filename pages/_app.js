fa58a16ff1df80fb87d376a4c7ade2cf342ed988
joel alexander <wksx9d@gmail.com>
2025-11-23 13:18:07 -0500

    google

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
+import Head from "next/head";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="npqMfDds_0oMITa6Qlm3ZHoIUJI3uqq37w3VfBMmyUQ"
+        />
+      </Head>
+
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
 
 export default MyApp;
+
